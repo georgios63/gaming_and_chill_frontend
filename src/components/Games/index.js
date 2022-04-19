71a883e20ee1b91b5f1d69aec07b7273c7ce6e73
georgios63 <georgemilitsopoulos63@gmail.com>
2022-04-19 15:02:18 +0200

    added btns in the ui

--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -3,6 +3,11 @@ import React, { useEffect } from "react";
 import { fetchGames } from "../../store/games/actions";
 import { allGames, gamesLoading } from "../../store/games/selectors";
 import { useDispatch, useSelector } from "react-redux";
+import { IconContext } from "react-icons";
+import { AiOutlineStar, AiOutlineDownload } from "react-icons/ai";
+import { RiAddFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const Games = () => {
   const dispatch = useDispatch();
@@ -20,6 +25,17 @@ const Games = () => {
           ? games.map((game) => (
               <div className="card-game" key={game.id}>
                 <img alt="" src={game.thumbnail} />
+                <div className="icon-container">
+                  <Button className="add-to-library-button">+</Button>
+                  <IconContext.Provider
+                    value={{ className: "card-icons", size: 30 }}
+                  >
+                    <a href={game.game_url}>
+                      <AiOutlineDownload />
+                    </a>
+                    <AiOutlineStar />
+                  </IconContext.Provider>
+                </div>
               </div>
             ))
           : "loading"}
