23d276454bd6856ac2081cdccd71256237dddddc
georgios63 <georgemilitsopoulos63@gmail.com>
2022-04-19 15:59:22 +0200

    added hover pop up msg on btns

--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -1,18 +1,25 @@
 import "./styles.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { fetchGames } from "../../store/games/actions";
 import { allGames, gamesLoading } from "../../store/games/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { IconContext } from "react-icons";
 import { AiOutlineStar, AiOutlineDownload } from "react-icons/ai";
-import { RiAddFill } from "react-icons/ri";
-import { Link } from "react-router-dom";
-import Button from "react-bootstrap/Button";
+import { OverlayTrigger, Tooltip, Button } from "react-bootstrap";
 
 const Games = () => {
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+
   const dispatch = useDispatch();
   const loading = useSelector(gamesLoading);
   const games = useSelector(allGames);
+
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Add to your library
+    </Tooltip>
+  );
 
   useEffect(() => {
     dispatch(fetchGames);
@@ -26,7 +33,25 @@ const Games = () => {
               <div className="card-game" key={game.id}>
                 <img alt="" src={game.thumbnail} />
                 <div className="icon-container">
-                  <Button className="add-to-library-button">+</Button>
+                  <OverlayTrigger
+                    placement="bottom"
+                    delay={{ show: 250, hide: 100 }}
+                    overlay={renderTooltip}
+                  >
+                    <Button
+                      variant="secondary"
+                      style={{
+                        alignSelf: "center",
+                        border: "1px solid black",
+                        borderRadius: "50px",
+                        margin: "15px",
+                        width: "50px",
+                        height: "50px",
+                      }}
+                    >
+                      +
+                    </Button>
+                  </OverlayTrigger>
                   <IconContext.Provider
                     value={{ className: "card-icons", size: 30 }}
                   >
