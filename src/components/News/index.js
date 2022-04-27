ceaafcad29c709231c97ea7c05b6a95b896ae110
georgios63 <georgemilitsopoulos63@gmail.com>
2022-04-27 17:22:52 +0200

    added news and styling on user page

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,3 +1,4 @@
+import "./styles.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchNews } from "../../store/news/actions";
@@ -14,14 +15,50 @@ const News = () => {
   }, []);
   return (
     <div className="news-container">
-      <div>
-        <h1>News</h1>
+      <div
+        style={{
+          margin: "40px",
+          display: "flex",
+          justifyContent: "center",
+          textShadow: "1px 1px 5px white",
+        }}
+      >
+        <h1>Latest News</h1>
       </div>
-      <div>
+      <div className="news-data-container">
         {!loading
           ? news.map((news) => (
-              <div>
-                <img alt="" src={news.thumbnail} />
+              <div
+                style={{
+                  border: "1px solid rgba(185, 180, 180, 0.158)",
+                  margin: "5px",
+                }}
+              >
+                <p
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    maxWidth: "570px",
+                  }}
+                >
+                  {news.title}
+                </p>
+                <div className="news-details-container">
+                  <img
+                    style={{ marginLeft: "5px" }}
+                    alt=""
+                    src={news.thumbnail}
+                  />
+                  <div>
+                    <p style={{ maxWidth: "350px", margin: "10px" }}>
+                      {news.short_description}
+                    </p>
+                    <p style={{ margin: "20px" }}>
+                      Click<a href={news.article_url}> HERE </a> to see the full
+                      article
+                    </p>
+                  </div>
+                </div>
               </div>
             ))
           : ""}
@@ -31,3 +68,5 @@ const News = () => {
 };
 
 export default News;
+
+//  <p dangerouslySetInnerHTML={{ __html: news.article_content }}></p>;
