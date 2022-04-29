48bfe51beb1c2fd6153eb6fa84b421fffa438f67
georgios63 <georgemilitsopoulos63@gmail.com>
2022-04-29 12:09:34 +0200

    deplying frontend

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const apiUrl =
-  process.env.API_URL || "https://gaming-and-chill.herokuapp.com/";
+  process.env.API_URL || "https://gaming-and-chill.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
